752ab9beec685c6268a1633dd64b80d92cb8ad7b
FinallRareStaff <Jeymester@gmail.com>
2022-04-08 01:27:33 +0300

    Implementation without hibernate.cfg.xml

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,9 +3,14 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
+import javax.imageio.spi.ServiceRegistry;
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -16,11 +21,21 @@ public class Util {
     }
     // Метод для создание фабрики по производству сессии
     private static SessionFactory createSessionFactory() {
-        SessionFactory factory = new Configuration()
-                .configure("hibernate.cfg.xml") // Для чтения конфига
-                .addAnnotatedClass(User.class) // Класс со специальными аннотациями
-                .buildSessionFactory(); // Метод для билда фабрики
-        System.out.println("Сессия создана");
-        return factory;
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/kata");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, "EBA14c86a74gEBA");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(User.class);
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
     }
 }
