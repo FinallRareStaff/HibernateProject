de5719cf0ea6cc8f44eaa8ccc5161b862334a9cc
FinallRareStaff <Jeymester@gmail.com>
2022-04-07 17:49:36 +0300

    CreateUserTable implemented

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -12,7 +14,19 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS User (" +
+                "ID int PRIMARY KEY AUTO_INCREMENT,"+
+                "NAME varchar(20) NOT NULL,"+
+                "LAST_NAME varchar(20) NOT NULL,"+
+                "AGE int(3) NOT NULL"+
+                ");";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction(); // Открытие транзакции
+            session.createSQLQuery(sql).addEntity(User.class).executeUpdate(); // Создание запроса sql
+            session.getTransaction().commit(); // Подтвердить изменения в БД
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
